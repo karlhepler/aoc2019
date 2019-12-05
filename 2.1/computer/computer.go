f02211b83196708b64c426a475e2128965cb55f3
Karl Hepler <karl.hepler@bronto.com>
2019-12-05 09:43:22 -0500

    refactor computer to use ExecLine

--- 2.1/computer/computer.go
+++ 2.1/computer/computer.go
@@ -8,18 +8,54 @@ const (
 	OpcodeMult = 2
 )
 
+type ErrorCode int
+
+const (
+	ErrorHalt ErrorCode = iota + 1
+	ErrorOpcode
+)
+
+type Error struct {
+	Code    ErrorCode
+	Message string
+}
+
+func (err Error) Error() string {
+	return err.Message
+}
+
 func Exec(prgm []int) ([]int, error) {
+	var err error
+
 	for i, num := 0, len(prgm); i < num; i += 4 {
-		switch prgm[i] {
-		case OpcodeHalt:
-			return prgm, nil
-		case OpcodeAdd:
-			prgm[prgm[i+3]] = prgm[prgm[i+1]] + prgm[prgm[i+2]]
-		case OpcodeMult:
-			prgm[prgm[i+3]] = prgm[prgm[i+1]] * prgm[prgm[i+2]]
-		default:
-			return prgm, fmt.Errorf("%v is an invalid opcode", prgm[i])
+		prgm, err = ExecLine(i, prgm)
+
+		if err == nil {
+			continue
 		}
+
+		switch err.(Error).Code {
+		case ErrorHalt:
+			return prgm, nil
+		case ErrorOpcode:
+			return prgm, err
+		}
+	}
+
+	return prgm, nil
+}
+
+func ExecLine(i int, prgm []int) ([]int, error) {
+	switch prgm[i] {
+	case OpcodeHalt:
+		return prgm, Error{Code: ErrorHalt}
+	case OpcodeAdd:
+		prgm[prgm[i+3]] = prgm[prgm[i+1]] + prgm[prgm[i+2]]
+	case OpcodeMult:
+		prgm[prgm[i+3]] = prgm[prgm[i+1]] * prgm[prgm[i+2]]
+	default:
+		msg := fmt.Sprintf("%v is an invalid opcode", prgm[i])
+		return prgm, Error{Code: ErrorOpcode, Message: msg}
 	}
 
 	return prgm, nil

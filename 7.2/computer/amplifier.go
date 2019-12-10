1a94f6880c8ae6e3e371f4af8a79aeb18b228574
Karl Hepler <karl.hepler@gmail.com>
2019-12-09 21:36:03 -0500

    added channel-based computer to 7.2

--- 7.2/computer/amplifier.go
+++ 7.2/computer/amplifier.go
@@ -1,53 +1,53 @@
 package computer
 
-import (
-	"github.com/karlhepler/aoc2019/5.2/computer"
-)
+// import (
+// 	"github.com/karlhepler/aoc2019/5.2/computer"
+// )
 
-func NewAmplifierChain(prgm []int, phaseSettings [5]int) (chain AmplifierChain) {
-	for i := range chain {
-		chain[i] = Amplifier{
-			Program:      clone(prgm),
-			PhaseSetting: phaseSettings[i],
-		}
-	}
-	return
-}
+// func NewAmplifierChain(prgm []int, phaseSettings [5]int) (chain AmplifierChain) {
+// 	for i := range chain {
+// 		chain[i] = Amplifier{
+// 			Program:      clone(prgm),
+// 			PhaseSetting: phaseSettings[i],
+// 		}
+// 	}
+// 	return
+// }
 
-type AmplifierChain [5]Amplifier
+// type AmplifierChain [5]Amplifier
 
-func (chain AmplifierChain) Exec(input int) (output int, err error) {
-	for range chain {
-		output, err = chain.exec(input)
-		if err != nil || output == 0 {
-			return
-		}
-		input = output
-	}
-	return
-}
+// func (chain AmplifierChain) Exec(input int) (output int, err error) {
+// 	for range chain {
+// 		output, err = chain.exec(input)
+// 		if err != nil || output == 0 {
+// 			return
+// 		}
+// 		input = output
+// 	}
+// 	return
+// }
 
-func (chain AmplifierChain) exec(input int) (output int, err error) {
-	for _, amp := range chain {
-		if input, err = amp.Exec(input); err != nil {
-			return
-		}
-	}
+// func (chain AmplifierChain) exec(input int) (output int, err error) {
+// 	for _, amp := range chain {
+// 		if input, err = amp.Exec(input); err != nil {
+// 			return
+// 		}
+// 	}
 
-	output = input
-	return
-}
+// 	output = input
+// 	return
+// }
 
-type Amplifier struct {
-	Program      []int
-	PhaseSetting int
-}
+// type Amplifier struct {
+// 	Program      []int
+// 	PhaseSetting int
+// }
 
-func (amp Amplifier) Exec(input int) (output int, err error) {
-	output, err = computer.Exec(amp.Program, amp.PhaseSetting, input)
-	return
-}
+// func (amp Amplifier) Exec(input int) (output int, err error) {
+// 	output, err = computer.Exec(amp.Program, amp.PhaseSetting, input)
+// 	return
+// }
 
-func clone(prgm []int) []int {
-	return append(prgm[:0:0], prgm...)
-}
+// func clone(prgm []int) []int {
+// 	return append(prgm[:0:0], prgm...)
+// }
---
+++ 7.2/computer/computer.go
@@ -0,0 +1,106 @@
+package computer
+
+import (
+	"fmt"
+
+	comp2 "github.com/karlhepler/aoc2019/2.1/computer"
+	comp51 "github.com/karlhepler/aoc2019/5.1/computer"
+	comp52 "github.com/karlhepler/aoc2019/5.2/computer"
+)
+
+type Output struct {
+	Value int
+	Error error
+}
+
+func NewComputer(prgm []int) Computer {
+	return Computer{prgm}
+}
+
+type Computer struct {
+	Program []int
+}
+
+func (comp Computer) Exec(input <-chan int, output chan<- Output) {
+	for i, num := 0, len(comp.Program); i < num; {
+		opcode, modes, err := comp51.ParseOpcode(comp.Program[i])
+		if err != nil {
+			output <- Output{Value: -1, Error: err}
+			return
+		}
+
+		switch opcode {
+		case comp2.OpcodeHalt:
+			output <- Output{}
+			return
+
+		case comp2.OpcodeAdd:
+			params := [3]*int{&comp.Program[i+1], &comp.Program[i+2], &comp.Program[i+3]}
+			if err := comp51.Add(&comp.Program, modes, params); err != nil {
+				output <- Output{Value: -1, Error: err}
+				return
+			}
+			i += 4
+
+		case comp2.OpcodeMult:
+			params := [3]*int{&comp.Program[i+1], &comp.Program[i+2], &comp.Program[i+3]}
+			if err := comp51.Multiply(&comp.Program, modes, params); err != nil {
+				output <- Output{Value: -1, Error: err}
+				return
+			}
+			i += 4
+
+		case comp51.OpcodeInput:
+			comp.Program[comp.Program[i+1]] = <-input
+			i += 2
+
+		case comp51.OpcodeOutput:
+			val, err := comp51.Output(&comp.Program, modes, &comp.Program[i+1])
+			if err != nil {
+				output <- Output{Value: -1, Error: err}
+				return
+			}
+			if val != 0 {
+				output <- Output{Value: val}
+				return
+			}
+			i += 2
+
+		case comp52.OpcodeJumpTrue:
+			params := [2]*int{&comp.Program[i+1], &comp.Program[i+2]}
+			if err := comp52.JumpIf(true, &i, &comp.Program, modes, params); err != nil {
+				output <- Output{Value: -1, Error: err}
+				return
+			}
+
+		case comp52.OpcodeJumpFalse:
+			params := [2]*int{&comp.Program[i+1], &comp.Program[i+2]}
+			if err := comp52.JumpIf(false, &i, &comp.Program, modes, params); err != nil {
+				output <- Output{Value: -1, Error: err}
+				return
+			}
+
+		case comp52.OpcodeLessThan:
+			params := [3]*int{&comp.Program[i+1], &comp.Program[i+2], &comp.Program[i+3]}
+			if err := comp52.Compare(comp52.LessThan, &comp.Program, modes, params); err != nil {
+				output <- Output{Value: -1, Error: err}
+				return
+			}
+			i += 4
+
+		case comp52.OpcodeEquals:
+			params := [3]*int{&comp.Program[i+1], &comp.Program[i+2], &comp.Program[i+3]}
+			if err := comp52.Compare(comp52.Equals, &comp.Program, modes, params); err != nil {
+				output <- Output{Value: -1, Error: err}
+				return
+			}
+			i += 4
+
+		default:
+			output <- Output{Value: -1, Error: fmt.Errorf("%v is an invalid opcode", comp.Program[i])}
+			return
+		}
+	}
+
+	output <- Output{Value: -1}
+}

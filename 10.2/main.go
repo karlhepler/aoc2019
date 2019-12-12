eb463e3e16e9833bcd18249f2bc24257aeb4132f
Karl Hepler <karl.hepler@gmail.com>
2019-12-11 23:44:31 -0500

    added timing

--- 10.2/main.go
+++ 10.2/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/karlhepler/aoc2019/10.2/asteroid"
 	"github.com/karlhepler/aoc2019/input"
 )
 
 func main() {
+	start := time.Now()
+
 	m := asteroid.BuildMap(input.Lines("10.1"))
 	station, visible := asteroid.MonitoringStation(m)
 
 	log.Println(station, len(visible))
+
+	fmt.Printf("Time: %v\n", time.Since(start))
 }

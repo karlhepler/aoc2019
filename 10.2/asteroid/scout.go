0fa1d5f7bfcfcbc796b48d9033885d50ff79940f
Karl Hepler <karl.hepler@gmail.com>
2019-12-11 23:42:30 -0500

    made it concurrent

--- 10.2/asteroid/scout.go
+++ 10.2/asteroid/scout.go
@@ -4,38 +4,53 @@ import (
 	"sort"
 )
 
-// MonitoringStation returns the map coordinates of the monitoring station.
+// MonitoringStation returns the map coordinates of the monitoring station and
+// a slice of asteroids that are visible from it.
 func MonitoringStation(m Map) (station Coord, visible []Coord) {
-	var maxlen int
+	res := make(chan data)
 
 	for _, ast := range m {
-		tm := m.Translate(ast)
-		vis := Visible(tm)
-		if maxlen < len(vis) {
-			maxlen = len(vis)
-			station = ast
-			visible = vis
+		go Visible(ast, m.Translate(ast), res)
+	}
+
+	i := 0
+	maxlen := 0
+	for d := range res {
+		if maxlen < len(d.visible) {
+			maxlen = len(d.visible)
+			station = d.station
+			visible = d.visible
+		}
+
+		i++
+		if i == len(m) {
+			close(res)
 		}
 	}
 
 	return
 }
 
+type data struct {
+	station Coord
+	visible []Coord
+}
+
 // Visible returns a slice of coordinates to visible asteroids
-func Visible(m Map) []Coord {
+func Visible(station Coord, omap Map, res chan<- data) {
 	visible := make([]Coord, 0)
 
 	// Remove the origin
-	m = m.Without(Coord{0, 0})
+	omap = omap.Without(Coord{0, 0})
 
 	// Sort the map by clockwise angle
-	sort.Sort(byClockwiseAngle(m))
+	sort.Sort(byClockwiseAngle(omap))
 
 	// Loop through the map by clockwise angle
-	for _, ast := range m {
+	for _, ast := range omap {
 		// Get all of the asteroids at the current clockwise angle, sort them
 		// by distance, and choose the first (closest) one.
-		matches := m.AsteroidsAtClockwiseAngle(ast.ClockwiseAngle())
+		matches := omap.AsteroidsAtClockwiseAngle(ast.ClockwiseAngle())
 		sort.Sort(byDistance(matches))
 
 		// Append if it doesn't already exist
@@ -51,5 +66,5 @@ func Visible(m Map) []Coord {
 		}
 	}
 
-	return visible
+	res <- data{station, visible}
 }
